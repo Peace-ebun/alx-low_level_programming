e5e283b1a6d8352ef46665248244560ef4e00576
Peace-ebun <peaceagbenuko409@gmail.com>
2022-09-22 23:07:53 -0700

    functions

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -5,7 +5,7 @@ include "main.h"
  * @n1: first number
  * @n2: second number
  * @r: buffer for result
- * @size_r: buffer siz
+ * @size_r: buffer size
  *
  * Return: address of r or 0
  */
@@ -45,3 +45,5 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	}
 	return (r);
 }
+
+			
